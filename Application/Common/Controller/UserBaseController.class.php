bcd5194a6763bb15cbd1e273fdaf7bd2394bbda9
iflyBird <sessonCookies@163.com>
2018-01-01 21:44:57 +0800

    实现了用户的权限管理和验证

--- Application/Common/Controller/UserBaseController.class.php
+++ Application/Common/Controller/UserBaseController.class.php
@@ -11,6 +11,17 @@ use Think\Controller;
 
 class UserBaseController extends Controller
 {
+
+//开启权限认证(暂时不开)
+  /*  public function _initialize()
+    {
+        // 判断用户是否有权限登录管理员后台
+        $auth = new \Think\Auth();
+        $result = $auth->check('User/*', $_SESSION['userId']);
+        if (!$result) {
+            header('location:' . U('User/Login/login'));
+        }
+    }*/
     public function display()
     {
         if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
